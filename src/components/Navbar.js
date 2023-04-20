c0617e0019c9a8ac816e8f79bd01ef5bd38f8ba7
mihirbpi <mihir.borkar@gmail.com>
2023-04-20 10:13:29 -0700

    update date

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar  = () => {
             <Link to="/resume">Resume</Link>
         </div>
         <div className="info">Created with React</div>
-        <div className="info">Last Updated March 2023</div>
+        <div className="info">Last Updated April 2023</div>
       </nav>
     );
 }
